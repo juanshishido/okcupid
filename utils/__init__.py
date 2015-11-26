c5e95a73f51b35b321a06d17ba178fb755303f84
juanshishido <juanshishido@cal.berkeley.edu>
2015-11-25 22:45:11 -0800

    Hashes: .py and .json

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .textnormalization import *
 from .happyfuntokenizing import Tokenizer
 from .rotate_factor import ortho_rotation
+from .checksums import *
---
+++ utils/checksums.py
@@ -0,0 +1,83 @@
+import os
+import json
+import pickle
+import hashlib
+
+import numpy as np
+
+
+def hash_update(f, h, force_update=False):
+    """Add a new filename and hash to `hashes.json`
+
+    Parameters
+    ----------
+    f : str
+        The filename (without a path) for the particular object
+    h : str
+        The hash for `f`
+    force_update : bool
+        Whether to replace 
+
+    Returns
+    -------
+    None
+
+    """
+    hashes = 'data/hashes.json'
+    kv = {f : h}
+    if os.path.isfile(hashes):
+        with open(hashes) as f:
+            data = json.load(f)
+
+        if f in data.keys():
+            if force_update:
+                data[f] = h
+        else:
+            data.update(kv)
+        with open(hashes, 'w') as f:
+            json.dump(data, f, indent=4)
+    else:
+        with open(hashes, 'w') as f:
+            json.dump(kv, f, indent=4)
+        
+
+def hash_get(f):
+    """Get the MD5 hash based on the rounded sum of the matrix values.
+
+    Parameter
+    ---------
+    f : str
+        The (relative path) filename for the particular object
+
+    Returns
+    -------
+    m.hexdigest: str
+        The digest containing only hexadecimal digits
+    """
+
+    X = pickle.load(open(f, 'rb'))
+    X = round(X.sum(), 8)
+    m = hashlib.md5()
+    m.update(X)
+    return m.hexdigest()
+    
+def hash_same(k):
+    """Get the hash and check it against what's in checksums.json.
+
+    Parameters
+    ----------
+    k : str
+        The filename (without a path) for the particular object
+
+    Returns
+    -------
+    valid : bool
+        True if the hashes match, False otherwise
+    """
+
+    j = json.load(open('data/hashes.json', 'r'))
+    if k in j.keys():
+        f = 'data/'+k
+        return hash_get(f) == j[k]
+    else:
+        raise ValueError('hash for {0} not in checksums.json'.format(k))
