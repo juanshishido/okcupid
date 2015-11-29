8dfca11810d76e2d6cd51e3431b9f6f0144057a5
juanshishido <juanshishido@cal.berkeley.edu>
2015-11-29 00:08:37 -0800

    Handling pca_dict_50.pkl, add hash

--- utils/checksums.py
+++ utils/checksums.py
@@ -44,7 +44,7 @@ def hash_update(f, h, force_update=False):
 def hash_get(f):
     """Get the MD5 hash based on the rounded sum of the matrix values.
 
-    Parameter
+    Parameters
     ---------
     f : str
         The (relative path) filename for the particular object
@@ -54,9 +54,11 @@ def hash_get(f):
     m.hexdigest: str
         The digest containing only hexadecimal digits
     """
-
     X = pickle.load(open(f, 'rb'))
-    X = round(X.sum(), 8)
+    if f == 'data/datamatrix.pkl':
+        X = round(X.sum(), 8)
+    elif f == 'data/pca_dict_50.pkl':
+        X = np.round(X['X_reduced'][0].sum(), 8)
     m = hashlib.md5()
     m.update(X)
     return m.hexdigest()
@@ -74,7 +76,6 @@ def hash_same(k):
     valid : bool
         True if the hashes match, False otherwise
     """
-
     j = json.load(open('data/hashes.json', 'r'))
     if k in j.keys():
         f = 'data/'+k
