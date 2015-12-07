2016de50c79ee77cc4ff14e50db3a6eb8dc48e55
Matar Haller <matarhaller@gmail.com>
2015-12-07 12:22:15 -0800

    moved clean_up into utils

--- utils/clean_up.py
+++ utils/clean_up.py
@@ -1,13 +1,8 @@
 import re
-import pickle
 import string
-
 import pandas as pd
 from bs4 import BeautifulSoup
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-from utils.hash import *
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from utils import happyfuntokenizing
 
 
@@ -20,29 +15,23 @@ def clean_up(df, col_names, min_words = 5):
     for c in col_names:
         df[c] = df[c].apply(lambda x: BeautifulSoup(x).getText().replace('\n', ' '))\
                 .apply(lambda x: re.sub('\s+', ' ', x).strip())
-        #drop rows where current essay has < min_words
-        token_count = df[c].str.split().str.len()
-        df = df[token_count > min_words]
+        token_count = df[c].str.split().str.len() 
+        df = df[token_count > min_words] #drop rows where current essay has < min_words
+    return df
 
-def create_data_matrix(df, col_name, ppath, force_update=False):
+def col_to_data_matrix(df, col_name):
     '''
     Tokenize and vectorize for a single essay column (given by col_name)
-    Returns a datamatrix from that column
+    Returnss two matrices (countvect and tfidf) from that column
     '''
     
     count_vect = CountVectorizer(stop_words='english',
                                  tokenizer=happyfuntokenizing.Tokenizer().tokenize,
-                                 ngram_range=(1, 3), analyzer='word')
+                                 ngram_range=(1, 3), analyzer='word', min_df = 0.01)
 
-    data_matrix = count_vect.fit_transform(df[col_name])
+    count_matrix = count_vect.fit_transform(df[col_name])
     
-    data_matrix_dense = data_matrix.todense().astype(float)
-                
-    with open(ppath, 'wb') as f:
-        pickle.dump(data_matrix_dense, f)
+    tfidf = TfidfTransformer()
+    tfidf_matrix = tfidf.fit_transform(count_matrix)
 
-    #add or update hash
-    hash_update(p, hash_get(p), force_update=force_update)
-        
-    return data_matrix
-
+    return count_matrix, tfidf_matrix
