2a33b685cdf492afbef09c624ea578d2287cad21
Jaya Narasimhan <jnaras@berkeley.edu>
2015-12-07 12:16:19 -0800

    Adding prep for pets, sign, language, ethnicity

--- utils/categorize_demographics.py
+++ utils/categorize_demographics.py
@@ -69,3 +69,60 @@ def drink_categories(drink):
         return 'unknown'
     else:
         return drink
+
+def language_categories(text):
+    if type(text) == float:
+	    return 'did not answer'
+    else:
+	    return text.count(',') + 1
+
+def pets_categories(text):
+    yay_cats = False
+    yay_dogs = False
+    no_cats = False
+    no_dogs = False
+    if type(text) == float:
+        return 'no answer'
+    if ('likes cats' in text or 'has cats' in text) and 'dislikes cats' not in text:
+        yay_cats = True
+    if ('likes dogs' in text or 'has dogs' in text) and 'dislikes dogs' not in text:
+        yay_dogs = True
+    if 'dislikes dogs' in text:
+        #print(text)
+        no_dogs = True
+        #print(no_dogs)
+    if 'dislikes cats' in text:
+        #print(text)
+        no_cats = True
+    if yay_cats and yay_dogs:
+        return 'likes both'
+    elif no_cats and no_dogs:
+        #print('dislikes both')
+        return 'dislikes both'
+    elif yay_cats:
+        return 'likes cats'
+    elif yay_dogs:
+        return 'likes dogs'
+    elif no_cats:
+        #print('no cats')
+        return 'dislikes cats'
+    elif no_dogs:
+        #print('no dogs')
+        return 'dislikes dogs'
+    return 'no answer'
+
+def ethnicity_categories(text):
+    if type(text) == float:
+        return 'no answer'
+    elif ',' in text:
+        return 'multi'
+    return text
+
+def sign_categories(text):
+    if type(text) == float:
+        return 'no answer'
+    line = text.split(' ')
+    if len(line) == 1:
+        return 'just sign'
+    else:
+        return ' '.join(line[2:])
