7583de59ec1a9eddb5d7d828d0e497174d5290ca
juanshishido <juanshishido@cal.berkeley.edu>
2015-12-09 11:42:27 -0800

    Add pipes between terms

--- utils/nonnegative_matrix_factorization.py
+++ utils/nonnegative_matrix_factorization.py
@@ -23,7 +23,7 @@ def _print_words(model, feature_names, n_top_words):
     """
     for topic_idx, topic in enumerate(model.components_):
         print("Group %d:" % topic_idx)
-        print(" ".join([feature_names[i]
+        print(" | ".join([feature_names[i]
             for i in topic.argsort()[ : -n_top_words-1 : -1]]))
         print()
     print()
