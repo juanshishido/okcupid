d1e030cb65dce04ef1d68e087e3d29e984b5f498
juanshishido <juanshishido@cal.berkeley.edu>
2016-02-24 13:17:40 -0800

    refactor `cleanup()`
    return multiple pd.DataFrame based on length of `essay_list`

--- utils/clean_up.py
+++ utils/clean_up.py
@@ -4,27 +4,35 @@ import string
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
-
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from utils import happyfuntokenizing
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 
-def clean_up(df, col_names, min_words = 5):
+from utils import happyfuntokenizing
+
+
+def clean_up(input_df, col_names, min_words = 5):
     '''
     Input : data frame and list of columns to clean up
     Returns: cleaned data frame (overwrites those columns)
     Drops user if any essay has < min_words number of words (default = 5)
     '''
+    assert isinstance(col_names, list), 'Must be type list'
+    assert isinstance(input_df, pd.DataFrame), 'Must be pd.DataFrame'
+    dfs = []
     for c in col_names:
+        df = input_df.copy()
         df[c] = df[c].replace(np.nan, '' , regex=True) \
-                    .apply(lambda x: BeautifulSoup(x).getText().replace('\n', ' '))\
-                    .replace('\n', ' ')\
-                    .apply(lambda x: re.sub('\s+', ' ', x).strip())
+                     .apply(lambda x: BeautifulSoup(x).getText().replace('\n', ' '))\
+                     .replace('\n', ' ')\
+                     .apply(lambda x: re.sub('\s+', ' ', x).strip())
         token_count = df[c].str.split().str.len() 
         df = df[token_count > min_words] #drop rows where current essay has < min_words
-
         df.fillna('', inplace=True)
-    return df
+        dfs.append(df)
+    if len(col_names) == 1:
+        return dfs[0]
+    else:
+        return tuple(dfs)
 
 def col_to_data_matrix(df, col_name):
     '''
