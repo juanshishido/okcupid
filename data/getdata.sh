c1a54bfe3ce25bc24ef650648eac21c5b39d3b5f
Juan Shishido <juanshishido@cal.berkeley.edu>
2016-06-04 21:11:12 -0700

    update data source

--- data/getdata.sh
+++ data/getdata.sh
@@ -1,2 +1,4 @@
-wget https://github.com/everett-wetchler/okcupid/raw/master/profiles.20120630.csv.zip
-unzip profiles.20120630.csv
+wget https://github.com/rudeboybert/JSE_OkCupid/raw/master/profiles.csv.zip
+unzip profiles.csv
+mv profiles.csv profiles.20120630.csv # to match the previous file name
+rm profiles.csv.zip # clean up
