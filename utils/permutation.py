73430507cab990059b9f3887c130e778fca3608e
Juan Shishido <juanshishido@cal.berkeley.edu>
2016-06-06 23:02:04 -0700

    comparison predictions or means

--- utils/permutation.py
+++ utils/permutation.py
@@ -2,7 +2,25 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 
-def permute(a, b, permutations=10000):
+def _diff_means(m, arr):
+    """Calculate the difference-in-means statistic.
+    This is based on an input array, `arr`, where the first
+    `m` observations correspond to a particular class.
+
+    Parameters
+    ----------
+    m : int
+        Number of samples in the first class
+    arr : np.ndarray
+        Data for both classes
+
+    Returns
+    -------
+    float
+    """
+    return np.mean(arr[:m]) - np.mean(arr[m:])
+
+def permute(a, b, comparison='predictions', permutations=10000):
     """Estimate of the permutation-based p-value
 
     Parameters
@@ -13,6 +31,8 @@ def permute(a, b, permutations=10000):
     b : np.ndarray
         Data for another class or
         predicted labels, as returned by a classifier
+    comparison : str
+        {'predictions', 'means'}
     permutations : int, optional
         Number of permutations
 
@@ -21,14 +41,25 @@ def permute(a, b, permutations=10000):
     p_value : float
         The proportion of times a value as extreme
         as the observed estimate is seen
+
+    Notes
+    -----
+    This calculates the two-tailed p-value
     """
     assert isinstance(a, np.ndarray) and isinstance(b, np.ndarray)
+    assert comparison in ('predictions', 'means')
     np.random.seed(42)
-    b = b.copy()
-    baseline = accuracy_score(a, b)
+    if comparison == 'predictions':
+        c = b.copy()
+        compare = accuracy_score
+    else:
+        c = np.append(a, b)
+        a = a.shape[0]
+        compare = _diff_means
+    baseline = compare(a, c)
     v = []
     for _ in range(permutations):
-        np.random.shuffle(b)
-        v.append(accuracy_score(a, b))
+        np.random.shuffle(c)
+        v.append(compare(a, c))
     p_value = (np.array(v) >= baseline).sum() / permutations
     return p_value
