d2c29c47f854904971ee1c6a528549b071eb340d
Juan Shishido <juanshishido@cal.berkeley.edu>
2016-06-08 22:38:01 -0700

    spaCy tokenizer

---
+++ utils/spacy_tokenizer.py
@@ -0,0 +1,28 @@
+import string
+
+from spacy.en import English
+
+
+nlp = English(tagger=False, parser=False, entity=False)
+
+def spacy_tokenize(text):
+    """Use spaCy's default tokenizer, which can
+    handle "emoticons and other web-based features,"
+    and remove punctuation from the results
+    
+    Parameters
+    ----------
+    text : str
+        The text to tokenize
+        
+    Returns
+    -------
+    list of tokens (as strings)
+    
+    Notes
+    -----
+    Source of above quote:
+        https://nicschrading.com/project/Intro-to-NLP-with-spaCy/
+    """
+    tokens = nlp(text)
+    return [str(t) for t in tokens if str(t) not in string.punctuation]
